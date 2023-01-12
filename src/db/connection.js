c9c41d9f1e200a237a46d00f8b8717fa7f1060bd
S. Patterson <bountyfiveo@gmail.com>
2023-01-12 01:48:30 -0500

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV || "development" || "https://3.134.238.10";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
