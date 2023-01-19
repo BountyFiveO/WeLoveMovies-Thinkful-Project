d14e0701a12999fe5635b557088c3ef2f26119c2
BountyFiveO <bountyfiveo@gmail.com>
2023-01-18 21:19:53 -0500

    new file:   .vscode/settings.json 	modified:   knexfile.js 	modified:   src/app.js 	modified:   src/db/connection.js

--- knexfile.js
+++ knexfile.js
@@ -2,20 +2,19 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL = "postgres://qrkkvsmg:KTxvDy5jcKyRVkQA2h64AQ5cIyhLMQ1k@salt.db.elephantsql.com/qrkkvsmg",
-  PRODUCTION_DATABASE_URL = "postgres://kqiseawp:D8CWmmI3JsJUnDHG0_g3gvgG3EOS7rhT@isilo.db.elephantsql.com/kqiseawp",
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
 } = process.env;
 const URL =
     NODE_ENV === "production"
-    ? PRODUCTION_DATABASE_URL
-    : DEVELOPMENT_DATABASE_URL;
-
+    ? "PRODUCTION_DATABASE_URL"
+    : "DEVELOPMENT_DATABASE_URL";
+    console.log(process.env.PRODUCTION_DATABASE_URL)
 module.exports = {
  production: {
     client: "postgresql",
     connection: URL,
-    pool: { min: 0, max: 6 },
+    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
@@ -27,7 +26,7 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: URL,
-    pool: { min: 0, max: 6 },
+    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ if (process.env.USER) require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const notFound = require("./Errors/notFound");
 const reviewsRouter = require("./reviews/reviews.router");
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const errorHandler = require("./Errors/errorHandler");
+const notFound = require("./Errors/notFound");
 
 app.use(cors());
 app.use(express.json());
@@ -15,7 +15,7 @@ app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter)
 
-app.use(notFound);
 app.use(errorHandler);
+app.use(notFound);
 
 module.exports = app;
