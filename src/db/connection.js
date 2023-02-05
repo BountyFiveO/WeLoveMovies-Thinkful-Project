467dd825d307b75ece1c5b9ba1f848a597dba8fc
S. Patterson <bountyfiveo@gmail.com>
2023-02-05 00:26:04 -0500

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,4 +2,6 @@ const environment = process.env.NODE_ENV || "production";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
-module.exports = knex;
+export default knex;
+
+
