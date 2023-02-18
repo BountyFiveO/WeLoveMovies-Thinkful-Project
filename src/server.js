4c875b692b635a9dff856c95ff56e9c0f0e84366
BountyFiveO <bountyfiveo@gmail.com>
2023-02-17 22:23:40 -0500

    update

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
-const { PORT = 5001 } = process.env;
-import { listen } from "./app.js";
-import { migrate } from "./db/connection.js";
+import { PORT } from 'process.env';
+import app from './app.js';
+import knex from './db/connection.js';
+
+const PORT = 5001;
+
 const listener = () => console.log(`Listening on Port ${PORT}!`);
-migrate
+
+knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    listen(PORT, listener);
+    app.listen(PORT, listener);
   })
   .catch(console.error);
