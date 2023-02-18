8334f3a08df1abdea9470c53098e9f4223afefd1
S. Patterson <bountyfiveo@gmail.com>
2023-02-17 20:52:37 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 //   PRODUCTION_DATABASE_URL,
 // } = process.env;
 const URL = {
-NODE_ENV === "production" ? PRODUCTION_DATABASE_URL : DEVELOPMENT_DATABASE_URL;
+ NODE_ENV == "production" ? PRODUCTION_DATABASE_URL : DEVELOPMENT_DATABASE_URL;
 }
 module.exports = {
  production: {
