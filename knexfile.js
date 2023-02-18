0358d33e397192ad1da0a43ee85c78fd21986aaa
S. Patterson <bountyfiveo@gmail.com>
2023-02-17 20:59:14 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
  production: {
     client: "postgresql",
     connection: URL,
-    pool: { min: 1, max: 5 },
+    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
@@ -28,7 +28,7 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: URL,
-    pool: { min: 1, max: 5 },
+    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
