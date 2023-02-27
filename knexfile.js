73f3fdc92721eebc482e9af317ee0efad9c80ec3
S. Patterson <bountyfiveo@gmail.com>
2023-02-27 00:42:42 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,8 @@ const path = require("path");
 if (process.env.USER || process.env.USERNAME) require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://kqiseawp:9bXnOXLaNs9-k7JbEz3OL8KcJWdumZXL@isilo.db.elephantsql.com/kqiseawp",
 } = process.env;
-console.log(process.env.DATABASE_URL)
 module.exports = {
   development: {
     client: "postgresql",
