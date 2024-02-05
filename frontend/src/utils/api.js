6fefc3efa63b41c82c5f9ce79e32bd59e45bb274
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-05 03:08:23 -0600

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://216.24.57.3:10000";
+const API_BASE_URL = "https://movies.engineerpatterson.com:5001";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
